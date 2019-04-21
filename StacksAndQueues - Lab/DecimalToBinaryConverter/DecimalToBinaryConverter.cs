bb4ea7f03b5c32f3fe998a1049d670604bc9b996
Georgi Markov <markov_ood@abv.bg>
2019-04-22 00:22:26 +0300

    Completed Decimal to Binary Converter.

---
+++ StacksAndQueues - Lab/DecimalToBinaryConverter/DecimalToBinaryConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DecimalToBinaryConverter
+{
+    public class DecimalToBinaryConverter
+    {
+        public static void Main()
+        {
+            int decimalNumber = int.Parse(Console.ReadLine());
+
+            if (decimalNumber > 0)
+            {
+                Stack<int> binaryRepr = new Stack<int>();
+                while (decimalNumber > 0)
+                {
+                    binaryRepr.Push(decimalNumber % 2);
+                    decimalNumber /= 2;
+                }
+
+                int length = binaryRepr.Count;
+                for (int i = 0; i < length; i++)
+                {
+                    Console.Write(binaryRepr.Pop());
+                }
+
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
