95da54edc4584e7ad2c841b98fa4a36200b22f5c
Georgi Markov <markov_ood@abv.bg>
2019-04-30 23:51:03 +0300

    Completed Reverse numbers with a stack

---
+++ StacksAndQueues - Exercises/ReverseNumbersWithAStack/ReverseNumbersWithAStack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseNumbersWithAStack
+{
+    public class ReverseNumbersWithAStack
+    {
+        public static void Main()
+        {
+            int[] numbers = Console.ReadLine().
+                Split(' ', StringSplitOptions.RemoveEmptyEntries).
+                Select(int.Parse).
+                ToArray();
+
+            Stack<int> stack = new Stack<int>(numbers);
+            foreach (var number in stack)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
