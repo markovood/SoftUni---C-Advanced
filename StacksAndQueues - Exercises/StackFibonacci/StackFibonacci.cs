e099eec207bdb583b8c9058cd2c87813a501f3be
Georgi Markov <markov_ood@abv.bg>
2019-05-19 00:11:41 +0300

    Completed Stack Fibonacci  task...

---
+++ StacksAndQueues - Exercises/StackFibonacci/StackFibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackFibonacci
+{
+    public class StackFibonacci
+    {
+        public static void Main()
+        {
+            int sequenceLength = int.Parse(Console.ReadLine());
+
+            Stack<long> sequence = new Stack<long>();
+            sequence.Push(0);
+            sequence.Push(1);
+            
+            for (int i = 0; i < sequenceLength - 1; i++)
+            {
+                long lastElement = sequence.Pop();
+                long elementBeforeLast = sequence.Peek();
+
+                sequence.Push(lastElement);
+                sequence.Push(lastElement + elementBeforeLast); 
+            }
+
+            Console.WriteLine(sequence.Pop());
+        }
+    }
+}
