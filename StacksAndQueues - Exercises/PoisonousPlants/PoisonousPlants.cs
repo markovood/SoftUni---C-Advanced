965f59630cd8f376ba86f49ef6a90d3e954dc8cc
Georgi Markov <markov_ood@abv.bg>
2019-05-23 11:20:50 +0300

    Completed Poisonous plants task without using a stack or a queue... with lists...

---
+++ StacksAndQueues - Exercises/PoisonousPlants/PoisonousPlants.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PoisonousPlants
+{
+    public class PoisonousPlants
+    {
+        public static void Main()
+        {
+            int numberOfPlants = int.Parse(Console.ReadLine());
+            List<int> plants = Console.ReadLine().
+                Split(" ", StringSplitOptions.RemoveEmptyEntries).
+                Select(int.Parse).
+                ToList();
+
+            List<int> indexesToRemove = new List<int>();
+
+            int days = 0;
+            while (true)
+            {
+                for (int i = 0; i < plants.Count - 1; i++)
+                {
+                    int plantToTheLeft = plants[i];
+                    int currentPlant = plants[i + 1];
+
+                    if (plantToTheLeft < currentPlant)
+                    {
+                        indexesToRemove.Add(i + 1);
+                    }
+                }
+
+                if (indexesToRemove.Count == 0)// must break when no dead plants found in the current day
+                {
+                    break;
+                }
+
+                int counter = 0;// balances the indexes that are changed, as we remove an element from plants
+                for (int i = 0; i < indexesToRemove.Count; i++)
+                {
+                    plants.RemoveAt(indexesToRemove[i] - counter);
+                    counter++;
+                }
+
+                days++;
+                indexesToRemove.Clear();
+            }
+
+            Console.WriteLine(days);
+        }
+    }
+}
