ea5dbb689a9871334e1dbd33d72b6349b305e034
Georgi Markov <markov_ood@abv.bg>
2019-07-15 20:43:33 +0300

    Completed Line numbers task...

---
+++ Streams - Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        public static void Main()
+        {
+            char dirSeparator = Path.DirectorySeparatorChar;
+            string inputFileName = "text.txt";
+            string outputFileName = "output.txt";
+            string pathIn = $"..{dirSeparator}..{dirSeparator}..{dirSeparator}{inputFileName}";
+            string pathOut= $"..{dirSeparator}..{dirSeparator}..{dirSeparator}{outputFileName}";
+
+            using (var reader = new StreamReader(pathIn))
+            {
+                using (var writer = new StreamWriter(pathOut))
+                {
+                    int lineNumber = 1;
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        writer.WriteLine($"Line {lineNumber}:" + line);
+                        lineNumber++;
+
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
