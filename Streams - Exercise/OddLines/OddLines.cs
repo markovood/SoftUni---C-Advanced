93ea5e5cf7235a4863f3652d6504bfd8c4c70e43
Georgi Markov <markov_ood@abv.bg>
2019-07-15 19:58:45 +0300

    Completed Odd lines task...

---
+++ Streams - Exercise/OddLines/OddLines.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        public static void Main()
+        {
+            string path = $"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}text.txt";
+
+            using (var reader = new StreamReader(path))
+            {
+                int lineCounter = 0;
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (lineCounter % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    lineCounter++;
+                    line = reader.ReadLine();
+                } 
+            }
+        }
+    }
+}
